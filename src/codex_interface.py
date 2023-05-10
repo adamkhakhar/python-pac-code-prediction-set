2d1c79f8753abd2c30ff4621f1427bdbf9039c86
ak <akhakhar@allegro.seas.upenn.edu>
2023-05-09 22:34:21 -0400

    adding documentaiton

--- src/codex_interface.py
+++ src/codex_interface.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import sys
+import pprint
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
@@ -12,7 +13,28 @@ PATH_TO_OPEN_AI_KEY = "/home/akhakhar/keys/OPEN_AI_API_KEY"
 openai.api_key = read_file(PATH_TO_OPEN_AI_KEY).strip("\n")
 
 
-def get_evaluation(prompt: str, logprobs=1, max_tokens=50, model="code-davinci-002"):
+def get_evaluation(
+    prompt: str,
+    logprobs: int = 1,
+    max_tokens: int = 50,
+    model: str = "code-davinci-002",
+) -> openai.openai_object.OpenAIObject:
+    """
+    Generate a completion for a given prompt using the specified OpenAI model.
+
+    This function uses the OpenAI API to generate a completion for a given prompt.
+    It allows for customization of the model used, the number of log probabilities to return,
+    and the maximum number of tokens in the generated completion.
+
+    Args:
+        prompt (str): The prompt to generate a completion for.
+        logprobs (int, optional): The number of most probable tokens to return for each position. Defaults to 1.
+        max_tokens (int, optional): The maximum number of tokens in the generated completion. Defaults to 50.
+        model (str, optional): The model to use for generating the completion. Defaults to "code-davinci-002".
+
+    Returns:
+        openai.openai_object.OpenAIObject: The generated completion.
+    """
     return openai.Completion.create(
         model=model,
         prompt=prompt,
@@ -27,9 +49,10 @@ def get_evaluation(prompt: str, logprobs=1, max_tokens=50, model="code-davinci-0
 
 if __name__ == "__main__":
     prompt = """
-    # Python
+    # Python3. Only respond with the code to complete the line.
     # return sum of list l of floats
     from typing import List
     def sum(l : List[int]) -> int:
         return"""
-    print(get_evaluation(prompt))
+    res = get_evaluation(prompt, model="davinci", logprobs=2)
+    pprint.pprint(res)
