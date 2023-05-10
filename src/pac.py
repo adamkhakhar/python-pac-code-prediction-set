674e07ce79102f743794049e0489f0b63e82b144
ak <akhakhar@allegro.seas.upenn.edu>
2023-05-09 23:28:51 -0400

    pac documentation

--- src/pac.py
+++ src/pac.py
@@ -1,7 +1,18 @@
 import numpy as np
 
 
-def compute_k(n, e, d):
+def compute_k(n: int, e: float, d: float) -> int:
+    """
+    Computes the maximum number of successes in a binomial distribution.
+
+    Args:
+        n (int): The number of trials.
+        e (float): The probability of success on each trial.
+        d (float): The desired cumulative probability.
+
+    Returns:
+        int: The maximum number of successes such that the cumulative probability does not exceed d.
+    """
     r = 0.0
     s = 0.0
     for h in range(n + 1):
